d2f79f5904f0c4c4a730e87c901b42984c00d4de
FelipeSabja20 <fe.sabja@duocuc.cl>
2024-06-26 10:24:16 -0400

    Add files via upload

---
+++ MenuAutores_Libros.py
@@ -0,0 +1,19 @@
+import json 
+def MenuLibro():
+    print("*******************************")
+    print("*        MUNDO LIBRO          *")
+    print("*******************************")
+    print("\n")
+    print("\n")
+    print("[1]   Mantenedor de autores ")
+    print("[2] - Reportes ")
+    print("[3] - Salir ")
+def MenuMantenedorAutores():
+    print("*******************************")
+    print("*      MANTENEDOR AUTORES     *")
+    print("*******************************")
+    print("[1] - Agregar autor ")
+    print("[2]   Editar autor ")
+    print("[3] - Eliminar autor ")
+    print("[4] - Listar autor ")
+    print("[5]   Volver ")
